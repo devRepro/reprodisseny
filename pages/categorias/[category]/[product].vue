7b4262fd7d2095058419070dedce330b97d11364
bitDevBcn <jofuentesle@gmail.com>
2025-04-01 06:47:41 +0200

    nav product start

---
+++ pages/categorias/[category]/[product].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const { category, product } = route.params
+
+const {data:product} = await useAsyncData('product', () =>
+
+)
+</script>
+<template>
+</template>
