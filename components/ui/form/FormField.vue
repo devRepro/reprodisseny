16bb66950e5ed17c4730a4ebb56ee9399f8a84fd
unknown <dev@reprodisseny.com>
2025-05-29 13:59:42 +0200

    update deploy

---
+++ components/ui/form/FormField.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Field, useField } from 'vee-validate'
+import { computed } from 'vue'
+
+const props = defineProps<{
+  name: string
+}>()
+
+const { errorMessage, value, meta } = useField(() => props.name)
+
+const error = computed(() => errorMessage.value)
+</script>
+
+<template>
+  <div>
+    <slot :field="{ name: props.name, value, error, meta }" :errors="[error]"></slot>
+  </div>
+</template>
